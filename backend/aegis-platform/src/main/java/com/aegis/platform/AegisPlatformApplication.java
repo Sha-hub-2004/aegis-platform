1ff78c2be44681ad0d7ef6035af16278aac50c02
Varshh09 <varshh09@example.com>
2026-07-27 07:52:50 +0530

    Initial Spring Boot project setup

---
+++ backend/aegis-platform/src/main/java/com/aegis/platform/AegisPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.aegis.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AegisPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AegisPlatformApplication.class, args);
+	}
+
+}
